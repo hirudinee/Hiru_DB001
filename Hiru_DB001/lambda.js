dcfc9536141d85e0e84cceb028c7db191b382219
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-05 13:03:30 +0530

    SQS issue 29

--- Hiru_DB001/lambda.js
+++ Hiru_DB001/lambda.js
@@ -1,32 +1,28 @@
 let AWS = require('aws-sdk');
+const sqs = new SL.AWS.SQS(AWS);
 let connectionManager = require('./ConnectionManager');
 let SL = require('@slappforge/slappforge-sdk');
 const rds = new SL.AWS.RDS(connectionManager);
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 
-
-	ddb.put({
-		TableName: 'testhiru',
-		Item: {
-			'Name': 'Hiru',
-			'ID': '001',
-			'sample': '01'
-		}
-	}, function (err, data) {
-		if (err) {
-			console.log(err);
-		} else {
-			//your logic goes here
-			console.log(data);
-		}
+	sqs.receiveMessage({
+		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/480964559519/Hiru_Test1201',
+		AttributeNames: ['SenderId'],
+		MaxNumberOfMessages: '1',
+		VisibilityTimeout: '30',
+		WaitTimeSeconds: '0'
+	}, function (receivedMessages) {
+		receivedMessages.forEach(message => {
+			// your logic to access each message through out the loop. Each message is available under variable message 
+			// within this block
+		})
+	}, function (error) {
+		// implement error handling logic here
 	});
 
-	rds.beginTransaction({
-		instanceIdentifier: 'hirudinee'
-	}, function (error, connection) {
-		if (error) { throw err; }
-	});
+
+
 
 
 	callback(null, 'Successfully executed');
